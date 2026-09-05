8e989be6385c4ae1b67fb5337cb4b88f2fc09d6d
jyotidercima <dercimajyoti@gmail.com>
2026-09-05 22:08:02 +0530

    created basic nav bar structure and added components file will need to work on each file ahead

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+function Navbar() {
+    return (
+        <>
+            <div>
+                <img src="react.svg" alt="logo" />
+                <ul>
+                    <li>About</li>
+                    <li>Skills</li>
+                    <li>Projects</li>
+                    <li>Experience</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
