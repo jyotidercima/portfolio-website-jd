3a62eb08defdb96c6fca403737e6bb9a25c4257c
jyotidercima <dercimajyoti@gmail.com>
2026-09-23 22:44:44 +0530

    Added working contact form with EmailJS working all good till now

--- client/src/sections/Contact.jsx
+++ client/src/sections/Contact.jsx
@@ -1,9 +1,14 @@
+import.meta.env
 import { useState } from 'react';
+import emailjs from '@emailjs/browser'
 import SocialLinks from '../components/SocialLinks';
 import '../style/Contact.css'
 
 function Contact() {
 
+    // console.log("Service ID:", import.meta.env.VITE_EMAILJS_SERVICE_ID)
+    // console.log("Template ID:", import.meta.env.VITE_EMAILJS_TEMPLATE_ID)
+    // console.log("Public KEY:", import.meta.env.VITE_EMAILJS_PUBLIC_KEY)
 
     const [formData, setFormData] = useState({
         name: "",
@@ -16,10 +21,13 @@ function Contact() {
         type: ""
     });
 
+    const [isSending, setIsSending] = useState(false);
+    const [IsSent, setIsSent] = useState(false);
+
     // const [status, setStatus] = useState("All ok");
 
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         // Check for empty fields
@@ -63,20 +71,65 @@ function Contact() {
         console.log(formData);
 
         // setStatus("Thank you! Your message is ready to be sent.")
+        setIsSent(false);
+        setIsSending(true);
 
-        setStatus({
-            message: "Thank you! Your message is ready to be sent.",
-            type: "success"
-        })
+        try {
+            const result = await emailjs.send(
+                import.meta.env.VITE_EMAILJS_SERVICE_ID,
+                import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+                {
+                    name: formData.name,
+                    email: formData.email,
+                    message: formData.message
+                },
+                import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+            );
+            console.log("Email sent ", result)
+
+            setIsSent(true);
+
+            setStatus({
+                message: "Thank you! Your message is ready to be sent.",
+                type: "success"
+            });
+
+            setFormData({
+                name: "",
+                email: "",
+                message: ""
+            });
+
+        }
+        catch (error) {
+            console.error("Email sending failed: ", error);
+
+            setStatus({
+                message: "Something went wrong. Please try agian.",
+                type: "error"
+            });
+        }
+        finally {
+            setIsSending(false);
+
+        }
+
+        // console.log("Email sent: ", result);
+
+        // setStatus({
+        //     message: "Thank you! Your message is ready to be sent.",
+        //     type: "success"
+        // })
 
         // console.log(formData);
 
-        setFormData({
-            name: "",
-            email: "",
-            message: ""
-        })
+        // setFormData({
+        //     name: "",
+        //     email: "",
+        //     message: ""
+        // })
     }
+
 
 
 
@@ -96,11 +149,11 @@ function Contact() {
 
                     <form action="" onSubmit={handleSubmit} className='main-form' noValidate>
                         <label htmlFor="name">Enter your good name:</label>
-                        <input type="text" name="name" id="user-name" value={formData.name} onChange={(e) => setFormData({ ...formData, name: e.target.value })} required />
+                        <input type="text" name="name" id="user-name" value={formData.name} onChange={(e) => { setIsSent(false); setFormData({ ...formData, name: e.target.value }) }} required />
                         <label htmlFor="email">Your mail id:</label>
-                        <input type="email" name="email" id="email" value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })} required />
+                        <input type="email" name="email" id="email" value={formData.email} onChange={(e) => { setIsSent(false); setFormData({ ...formData, email: e.target.value }) }} required />
                         <label htmlFor="msg">Your message for me</label>
-                        <textarea name="msg" id="msg" placeholder='Your message' rows="5" value={formData.message} onChange={(e) => setFormData({ ...formData, message: e.target.value })} required>
+                        <textarea name="msg" id="msg" placeholder='Your message' rows="5" value={formData.message} onChange={(e) => { setIsSent(false); setFormData({ ...formData, message: e.target.value }) }} required>
 
                         </textarea>
 
@@ -110,7 +163,9 @@ function Contact() {
                                 {status.message}
                             </p>}
 
-                        <button type='submit'>SEND</button>
+                        <button type='submit' disabled={isSending}>
+                            {isSending ? "Sending.." : IsSent ? "Sent" : "Send"}
+                        </button>
                     </form>
                 </div>
                 <div className="social-contact">
